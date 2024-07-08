4b78083a22f51a3119bce139b605f10766ba538c
meganbyrd22 <meganbyrd22@gmail.com>
2024-07-08 15:56:01 -0500

    chore: Install tailwind and create home component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = (props) => {
+
+    return (
+        <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500">
+            <h1 className="text-3xl font-bold">Summer recipes you have to try!</h1>
+        </div>
+    )
+}
+
+export default Home
