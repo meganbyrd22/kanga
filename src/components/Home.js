e1a60a77fe9b714b126135502b50bfb216fc47ea
meganbyrd22 <meganbyrd22@gmail.com>
2024-07-09 13:22:54 -0500

    fix: Ensure tailwind installation was successful

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
-const Home = (props) => {
+const Home = () => {
 
     return (
         <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500">
-            <h1 className="text-3xl font-bold">Summer recipes you have to try!</h1>
+            <h1 className="text-3xl font-bold text-blue-400">Summer recipes you have to try!</h1>
         </div>
     )
 }
